bde2d7178e49beccfff1cd35973b4307f03c65c4
SVolkov <volkov@cmd.su>
2021-05-20 21:19:30 +0300

    create counter button

--- 01-basics/01-CounterButtonDraft/script.js
+++ 01-basics/01-CounterButtonDraft/script.js
@@ -1,3 +1,18 @@
-// import Vue from './vendor/vue.esm.browser.js';
+import Vue from './vendor/vue.esm.browser.js';
 
 // Требуется создать Vue приложение
+let app = new Vue({
+  el: '#app',
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    counter() {
+      this.count++;
+    },
+  },
+});
+
+app.$mount('#app');
