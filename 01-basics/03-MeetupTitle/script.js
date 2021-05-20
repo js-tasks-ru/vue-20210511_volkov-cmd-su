5380d6eb699548ce88927ed12c260bdeef6bb5bc
SVolkov <volkov@cmd.su>
2021-05-20 23:45:52 +0300

    create radio buttons and meetups titles

--- 01-basics/03-MeetupTitle/script.js
+++ 01-basics/03-MeetupTitle/script.js
@@ -1,3 +1,33 @@
-// import Vue from './vendor/vue.esm.browser.js';
+import Vue from './vendor/vue.esm.browser.js';
 
 // Требуется создать Vue приложение
+let app = new Vue({
+  data() {
+    return {
+      radioButtons: [1, 2, 3, 4, 5],
+      picked: 1,
+      titleMeetup: '',
+    };
+  },
+  mounted() {
+    this.api();
+  },
+  watch: {
+    picked() {
+      this.api();
+    },
+  },
+  methods: {
+    api() {
+      let promise = fetch('https://course-vue.javascript.ru/api/meetups/' + this.picked);
+      promise.then(response => {
+        return response.text();
+      }).then(data => {
+        data = JSON.parse(data);
+        this.titleMeetup = data.title;
+      });
+    },
+  },
+});
+
+app.$mount('#app');
