bd030dad094fe0fd8d823b553f1c277f88edd773
SVolkov <volkov@cmd.su>
2021-05-26 21:23:41 +0300

    remove task

--- 02-components/02-MeetupDescription/MeetupDescription.js
+++
@@ -1,9 +0,0 @@
-const MeetupDescription = {
-  name: 'MeetupDescription',
-
-  // Входные параметры
-
-  template: `<p class="meetup-description">Описание должно быть здесь</p>`,
-};
-
-export default MeetupDescription;
