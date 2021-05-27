25702ee74b18abb648ec6c0c1796860f4014f8d5
javascriptru <67475495+jsru-1@users.noreply.github.com>
2021-05-27 21:07:09 +0300

    Added task MeetupCover

---
+++ 02-components/03-MeetupCover/MeetupCover.js
@@ -0,0 +1,10 @@
+const MeetupCover = {
+  name: 'MeetupCover',
+
+  template: `
+    <div class="meetup-cover" style="--bg-url: url('https://course-vue.javascript.ru/api/images/2')">
+        <h1 class="meetup-cover__title">Title</h1>
+    </div>`,
+};
+
+export default MeetupCover;
