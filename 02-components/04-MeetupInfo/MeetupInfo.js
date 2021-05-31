95dbea0d1261055a4538bcd09df17609025671ce
SVolkov <volkov@cmd.su>
2021-05-31 21:34:17 +0300

    date task

--- 02-components/04-MeetupInfo/MeetupInfo.js
+++ 02-components/04-MeetupInfo/MeetupInfo.js
@@ -1,19 +1,41 @@
 export default {
   name: 'MeetupInfo',
+  props: {
+    organizer: {
+      type: String,
+      required: true,
+    },
+    place: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+  },
+  computed: {
+    correctDate1() {
+      return this.date.toISOString().split('T')[0];
+    },
+    correctDate2() {
+      return this.date.toLocaleDateString(navigator.language, {year: 'numeric', month: 'long', day: 'numeric'});
+    },
+  },
 
   template: `
     <ul class="info-list">
       <li>
         <img class="icon info-list__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
+        {{ organizer }}
       </li>
       <li>
         <img class="icon info-list__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
+        {{ place }}
       </li>
       <li>
         <img class="icon info-list__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
+        <time :datetime="correctDate1">{{correctDate2}}</time>
       </li>
     </ul>`,
 };
