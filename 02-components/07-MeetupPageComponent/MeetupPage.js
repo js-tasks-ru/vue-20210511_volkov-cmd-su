698cb47bea999100b9c874df44e6eaa173efb4ce
javascriptru <67475495+jsru-1@users.noreply.github.com>
2021-06-02 19:38:49 +0300

    Added task MeetupPageComponent

---
+++ 02-components/07-MeetupPageComponent/MeetupPage.js
@@ -0,0 +1,10 @@
+// import MeetupView from './MeetupView.js';
+// import { MEETUP_ID, fetchMeetup } from './data.js';
+
+const MeetupPage = {
+  name: 'MeetupPage',
+
+  template: `<div></div>`,
+};
+
+export default MeetupPage;
